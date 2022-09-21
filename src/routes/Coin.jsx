aaea0d522b1f84b3aa0b6e81139b1517dd4ada87
eduardojigub <eduardobrunoit@gmail.com>
2022-09-21 20:58:09 -0300

    fetching new api Route for ID

--- src/routes/Coin.jsx
+++ src/routes/Coin.jsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const Coin = () => {
-  return <div>Coin</div>;
+  const [coin, setCoin] = useState({});
+
+  const url = `https://api.coingecko.com/api/v3/coins/bitcoin`;
+
+  useEffect(() => {
+    axios
+      .get(url)
+      .then((response) => {
+        setCoin(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>{coin.id}</h1>
+    </div>
+  );
 };
 
 export default Coin;
